9a7bc51c246f254365d0ea122aa70d70cd045009
vunk.lai <vunk.lai@zyxel.com.tw>
2024-01-08 17:37:40 +0800

    fix: add tailwind config to create custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "tailwindcss/colors";
+
 export default {
   content: ["./src/**/*.tsx"],
   darkMode: "class",
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        light: colors.sky[300],
+        main: colors.sky[500],
+        dark: colors.sky[700],
+      },
+    },
     extend: {
       container: {
         center: true,
